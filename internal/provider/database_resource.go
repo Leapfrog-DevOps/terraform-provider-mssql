7a0e3471173648076f1eb2a319269c8652e9f4d5
dipendrastha <dipendrashrestha1@lftechnology.com>
2025-08-30 23:56:49 +0545

    added functionality to create database in sql server

--- internal/provider/database_resource.go
+++ internal/provider/database_resource.go
@@ -3,22 +3,38 @@ package provider
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int32default"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	_ "github.com/microsoft/go-mssqldb"
 )
 
 // Ensure the implementation satisfies the expected interfaces.
 var (
-	_ resource.Resource = &databaseResource{}
+	_ resource.Resource              = &databaseResource{}
+	_ resource.ResourceWithConfigure = &databaseResource{}
 )
 
-// NewOrderResource is a helper function to simplify the provider implementation.
+// NewDatabaseResource a helper function to simplify the provider implementation.
 func NewDatabaseResource() resource.Resource {
 	return &databaseResource{}
 }
 
-// orderResource is the resource implementation.
+// maps to resource schema table
+type databaseResourceModel struct {
+	Name               types.String `tfsdk:"name"`
+	Collation          types.String `tfsdk:"collation"`
+	CompatibilityLevel types.Int32  `tfsdk:"compatibility_level"`
+	Owner              types.String `tfsdk:"owner"`
+	Id                 types.String `tfsdk:"id"`
+}
+
+// databaseResource is the resource implementation.
 type databaseResource struct {
 	client *sql.DB
 }
@@ -30,11 +46,85 @@ func (r *databaseResource) Metadata(_ context.Context, req resource.MetadataRequ
 
 // Schema defines the schema for the resource.
 func (r *databaseResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
-	resp.Schema = schema.Schema{}
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "MSSQL Database resource",
+		Attributes: map[string]schema.Attribute{
+			"name": schema.StringAttribute{
+				MarkdownDescription: "Database name",
+				Required:            true,
+			},
+			"collation": schema.StringAttribute{
+				MarkdownDescription: "Database collation",
+				Optional:            true,
+				Computed:            true,
+				Default:             stringdefault.StaticString("SQL_Latin1_General_CP1_CI_AS"),
+			},
+			"compatibility_level": schema.Int32Attribute{
+				MarkdownDescription: "Database compatibility level",
+				Optional:            true,
+				Computed:            true,
+				Default:             int32default.StaticInt32(150),
+			},
+			"owner": schema.StringAttribute{
+				MarkdownDescription: "Database owner",
+				Required:            true,
+			},
+			"id": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "Database identifier.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+		},
+	}
 }
 
 // Create creates the resource and sets the initial Terraform state.
 func (r *databaseResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data databaseResourceModel
+	diags := req.Plan.Get(ctx, &data)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	// Create database in MSSQL
+	name := data.Name.ValueString()
+	collation := data.Collation.ValueString()
+	compatibilityLevel := data.CompatibilityLevel.ValueInt32()
+	owner := data.Owner.ValueString()
+
+	// Check if owner exists
+	var userExists bool
+	checkOwnrStmt := "SELECT COUNT(*) FROM sys.syslogins WHERE name = @owner"
+	err := r.client.QueryRowContext(ctx, checkOwnrStmt, sql.Named("owner", owner)).Scan(&userExists)
+	if err != nil {
+		resp.Diagnostics.AddError("Error checking for owner user", fmt.Sprintf("Unable to check if '%s' exists: %s", owner, err.Error()))
+		return
+	}
+	if !userExists {
+		resp.Diagnostics.AddError("User does not exist", fmt.Sprintf("The '%s' user is required to create the database and assign ownership. Please ensure the '%s' user exists before proceeding.", owner, owner))
+		return
+	}
+	// Create Statement
+	createStmt := fmt.Sprintf(`
+							CREATE DATABASE %s 
+							COLLATE %s
+							WITH COMPATIBILITY_LEVEL=%d ;
+	`, name, collation, compatibilityLevel)
+	_, err = r.client.ExecContext(ctx, createStmt)
+	if err != nil {
+		resp.Diagnostics.AddError("Error creating database", err.Error())
+		return
+	}
+	// SQL statement to set the owner to 'sa'
+	setOwnerStmt := fmt.Sprintf("ALTER AUTHORIZATION ON DATABASE::appdb TO %s;", owner)
+	_, err = r.client.ExecContext(ctx, setOwnerStmt)
+	if err != nil {
+		resp.Diagnostics.AddError("Error creating database", err.Error())
+	}
+	data.Id = types.StringValue(data.Name.ValueString())
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...) // Save state
 }
 
 // Read refreshes the Terraform state with the latest data.
@@ -47,4 +137,19 @@ func (r *databaseResource) Update(ctx context.Context, req resource.UpdateReques
 
 // Delete deletes the resource and removes the Terraform state on success.
 func (r *databaseResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+}
+
+func (r *databaseResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+	client, ok := req.ProviderData.(*sql.DB)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *sql.DB, got :%T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return
+	}
+	r.client = client
 }
